a9820348d8adb52e67f82c60762872fc218994b4
Can Kologlu <mckologlu@gmail.com>
2022-11-18 15:41:23 -0500

    Did a function takes a name returns hello string

---
+++ hello.js
@@ -0,0 +1,13 @@
+/*const sayHello = function () {                        ** First function call
+  console.log("Hello, world")
+}
+
+sayHello();*/                    
+
+const sayHello = function(name) {
+  console.log("Hello, "+name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+sayHello("Hidalgo");
